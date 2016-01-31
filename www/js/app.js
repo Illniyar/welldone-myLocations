4e9bae0571e47de5f5cbaf81e0a145a3a9c98aeb
alon bar david <illniyar@gmail.com>
2016-01-31 21:05:11 +0200

    changed default route to category

--- www/js/app.js
+++ www/js/app.js
@@ -2,7 +2,7 @@
 angular.module('myLocations',['ui.router','ngTouch','map','root.page'])
     .config(function RouteConfig(rootPageRouteProvider,$stateProvider,$urlRouterProvider){
         rootPageRouteProvider.load('root');
-        $urlRouterProvider.otherwise('/location');
+        $urlRouterProvider.otherwise('/category');
     })
     .config(function ServiceConfig(mapProvider){
         mapProvider.load('AIzaSyCCpDsVlaan5P5-pKZmSN1Oao0f6892kfo');
